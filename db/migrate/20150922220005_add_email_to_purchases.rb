bc9ee7d24f0fa20e2d883b331faafc7a28bddf95
Simone <sjaschko@yahoo.com>
2015-09-23 18:51:52 +0000

    heroku problem with table creation

--- db/migrate/20150922220005_add_email_to_purchases.rb
+++ db/migrate/20150922220005_add_email_to_purchases.rb
@@ -1,11 +1,4 @@
 class AddEmailToPurchases < ActiveRecord::Migration
   def change
-    add_column :purchases, :email, :string
-    add_column :purchases, :amount, :integer
-    add_column :purchases, :description, :string
-    add_column :purchases, :currency, :string
-    add_column :purchases, :customer_id, :string
-    add_column :purchases, :card, :string
-    add_column :purchases, :product_id, :integer
   end
 end
