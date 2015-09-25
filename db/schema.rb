258a596811654b509358180412f4cc804a9af19f
Simone <sjaschko@yahoo.com>
2015-09-25 01:34:38 +0000

    Added products table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924211353) do
+ActiveRecord::Schema.define(version: 20150925012948) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,6 +45,17 @@ ActiveRecord::Schema.define(version: 20150924211353) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "author"
+    t.text     "description"
+    t.string   "sku"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "purchases", force: :cascade do |t|
     t.datetime "created_at",  null: false
