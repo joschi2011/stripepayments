f62c35ee9eb47dd67d1331b0b6a29f343d51849d
Simone <sjaschko@yahoo.com>
2015-09-28 18:36:48 +0000

    Implementing a product list page for multiple products

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly",
+copywriting_monthly = Product.create(title: "Copywriting Monthly",
     subtitle: "Ongoing Course", author: "Mattan Griffel",
     length: "6 Months", author_image_name: "teacher-image.jpg" ,
-    price: "4.99", sku: "GROHACK2", download_url: "https://s3.amazonaws.com/sjmke-stripepayments/Freezer+Meal+Workshop+Options.pdf",
+    price: "4.99", sku: "COPYWRITE2", download_url: "https://s3.amazonaws.com/sjmke-stripepayments/Freezer+Meal+Workshop+Options.pdf",
     details: "We'll charge you Monthly, for unlimited time to our mentoring team", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
-            <p>In this series, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+            <p>In this series, Mattan Griffel introduces you to the concept of copywriting and shares his favorite tips for getting started as a growth hacker.</p>
             <p><strong>What You'll Learn</strong></p>
             <ul class="no-indent">
-                <li>What is a growth hacker?</li>
-                <li>The 5 stages of the user lifecycle</li>
-                <li>How to apply the Lean Marketing Framework</li>
-                <li>Resources and tools you'll need to know</li>
+                <li>How to write killer copy</li>
+                <li>Seven secrets of direct copy ad sales</li>
             </ul>},
    author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
                 <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.
@@ -25,4 +23,18 @@ growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly",
                 <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
 
 
-
+analytics_monthly = Product.create(title: "Analytics Monthly",
+    subtitle: "Ongoing Course", author: "Mattan Griffel",
+    length: "6 Months", author_image_name: "teacher-image.jpg" ,
+    price: "4.99", sku: "ANALYTICS2", download_url: "https://s3.amazonaws.com/sjmke-stripepayments/Freezer+Meal+Workshop+Options.pdf",
+    details: "We'll charge you Monthly, for unlimited time to our mentoring team", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
+            <p>In this series, Mattan Griffel introduces you to the concept of copywriting and shares his favorite tips for getting started as a growth hacker.</p>
+            <p><strong>What You'll Learn</strong></p>
+            <ul class="no-indent">
+                <li>How to use mix panel effectively</li>
+                <li>How to improve your sales funnel using analtics </li>
+            </ul>},
+   author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
